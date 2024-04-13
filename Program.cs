79dc8e155618d65fce7c535cf158f9e5efc7f23d
pducdieu <pducdieu17102002@gmail.com>
2024-04-13 23:00:37 +0700

    The new code function

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SumDivideTen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhập số thứ nhất:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Nhập số thứ hai:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double sum = num1 + num2;
+
+            double result = sum % 10;
+
+            Console.WriteLine("Tổng của " + num1 + "và" + num2 + "là:" + sum);
+            Console.WriteLine("Kết quả sau khi chia cho 10 là:" + result);
+
+            Console.ReadLine();
+        }
+    }
+}
